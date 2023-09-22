97a612ad81fb0bbeb86bbdb2da64a4607c62df6d
Ndahimana Bonheur <bonheurndahimana125@gmail.com>
2023-09-22 12:28:42 +0200

    Done out categories

--- php/shop-by-category.php
+++ php/shop-by-category.php
@@ -1,32 +1,35 @@
-<?php
-    if(mysqli_num_rows($get_category) < 1) {
-        ?>
-        <div class="form-results">
-            <div class="failed">
-                Error! The category's information is not found
-            </div>
-        </div>
-        <?php
-    }
-    else {
-    $data_category = mysqli_fetch_array($get_category);
-        $get_allproducts = mysqli_query($server,"SELECT * from products WHERE product_genre='$category'");
-        if (mysqli_num_rows($get_allproducts) < 1) {
+<div class="latest-products">
+    <?php
+        if(mysqli_num_rows($get_category) < 1) {
             ?>
             <div class="form-results">
-                <div class="latestproducts">
-                    <h1>
-                        Category '<b><?php echo $data_category['genre_name'] ?></b>'
-                    </h1>
-                </div>
                 <div class="failed">
-                    No products found!
+                    Error! The category's information is not found!
                 </div>
             </div>
             <?php
         }
         else {
-            include('php\products-by-category.php');
-        }
-    }
-?>
+            $data_category = mysqli_fetch_array($get_category);
+            $get_allproducts = mysqli_query($server,"SELECT * from products WHERE product_genre='$category'");
+            ?>
+            <h1>
+                CATEGORY: &nbsp;&nbsp; '<?php echo $data_category['genre_name'] ?>'
+            </h1>
+            <hr>
+            <?php
+                if (mysqli_num_rows($get_allproducts) < 1) {
+                        ?>
+                        <div class="form-results">
+                            <div class="failed">
+                                No products found!
+                            </div>
+                        </div>
+                        <?php
+                    }
+                    else {
+                        include('php\products-by-category.php');
+                    }
+                }
+    ?>
+</div>
--- shop-by-category.php
+++ shop-by-category.php
@@ -17,21 +17,20 @@
 <?php  
     include('php\head-tag.php');
 ?>
+<head>
+    <!-- Add the Bootstrap CSS link here -->
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> -->
+    <!-- Font awesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styling/main-style.css">
+</head>
 <body>
     <?php
-        include('php/modal-pages.php')
+        include('php/modal-pages.php');
+        include('php/out_header.php');
+        include('php/shop-by-category.php');
+        include('php/big-footer.php'); 
+        include('php/developer-footer.php');
     ?>
-    <div class="main-gate">
-        <?php
-            include('php/out_header.php');
-        ?>
-        <div class="remaining">
-            <?php
-                include('php/shop-by-category.php');
-                include('php/big-footer.php'); 
-                include('php/developer-footer.php');
-            ?>
-        </div>
-    </div>
 </body>
 </html>
