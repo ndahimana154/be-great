11f4891a725676861508e688d49a51f91cea4a2d
Ndahimana Bonheur <bonheurndahimana125@gmail.com>
2023-09-22 11:45:59 +0200

    Redesigned

--- Buyer-signup.php
+++ Buyer-signup.php
@@ -8,154 +8,148 @@
 <?php  
     include('php\head-tag.php');
 ?>
+<head>
+    <!-- Font awesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styling/main-style.css">
+</head>
 <body>
     <?php
-        include('php/modal-pages.php')
+        include('php/modal-pages.php');
+        include('php/out_header.php');
     ?>
-    <div class="main-gate">
-        <?php
-            include('php/out_header.php');
-        ?>
-        <div class="remaining">
-            <div class="signupform-box">
-                <div class="signup-cont">
-                    <div class="header">
-                        <h1>
-                            Buyer - Signup
-                        </h1>
-                        <a>
-                            <i class="fa fa-window-close"></i>
-                        </a>
-                    </div> 
-                    <div class="form-results">
-                        <?php
-                            if (isset($_POST['signupbtn'])) {
-                                $firstname = $_POST['firstN'];
-                                $lastname = $_POST['lastN'];
-                                $email = $_POST['emailA'];
-                                $phone = $_POST['phoneN'];
-                                $password = $_POST['passW']; 
-                                $retype = $_POST['retp'];
-                                // echo "dij";
-                                $profile = $_FILES['profilepicture']['name'];
-                                $phototype = $_FILES['profilepicture']['type'];
-                                $phototmp_name = $_FILES['profilepicture']['tmp_name'];
+    <div class="buyer-sign-cont">
+        <div class="buyer-sign-wrapper">
+            <div class="sign-form-cont">
+                <div class="header">
+                    <h1>
+                        Buyer - Signup
+                    </h1>
+                </div> 
+                <div class="form-results">
+                    <?php
+                        if (isset($_POST['signupbtn'])) {
+                            $firstname = $_POST['firstN'];
+                            $lastname = $_POST['lastN'];
+                            $email = $_POST['emailA'];
+                            $phone = $_POST['phoneN'];
+                            $password = $_POST['passW']; 
+                            $retype = $_POST['retp'];
+                            // echo "dij";
+                            $profile = $_FILES['profilepicture']['name'];
+                            $phototype = $_FILES['profilepicture']['type'];
+                            $phototmp_name = $_FILES['profilepicture']['tmp_name'];
 
-                                $photo_explode = explode('.', $profile);
-                                $photoexte = end($photo_explode);
+                            $photo_explode = explode('.', $profile);
+                            $photoexte = end($photo_explode);
 
-                                $accepted_exte = ["image/jpeg", "image/jpg", "image/png", "image/HEIC"];
-                                $check_email_ext = mysqli_query($server,"SELECT * from buyers WHERE email='$email'");
-                                if (mysqli_num_rows($check_email_ext) > 0) {
+                            $accepted_exte = ["image/jpeg", "image/jpg", "image/png", "image/HEIC"];
+                            $check_email_ext = mysqli_query($server,"SELECT * from buyers WHERE email='$email'");
+                            if (mysqli_num_rows($check_email_ext) > 0) {
+                                ?>
+                                <div class="failed">
+                                    Error - email(<b><?php echo $email ?></b>) already exists!
+                                </div>
+                                <?php
+                            }
+                            elseif ($password != $retype) {
+                                ?>
+                                <div class="failed">
+                                    Error - Passwords doesn't match!
+                                </div>
+                                <?php
+                            }
+                            elseif (in_array($phototype, $accepted_exte) == true) {
+                                $newprofileimagename = $email." - Profile image .png";
+                                $upload_pic = move_uploaded_file($phototmp_name,"images/buyers/profileimages/$newprofileimagename");
+                                $insert_buyer = mysqli_query($server,"INSERT into buyers values(null,'$firstname','$lastname','$email','$phone','$newprofileimagename','$password')");
+                                if ($upload_pic && $insert_buyer) {
                                     ?>
-                                    <div class="failed">
-                                        Error - email(<b><?php echo $email ?></b>) already exists!
+                                    <div class="succed">
+                                        Congratulations the account is created successfully!
+                                            - <?php echo $email ?> 
                                     </div>
                                     <?php
-                                }
-                                elseif ($password != $retype) {
-                                    ?>
-                                    <div class="failed">
-                                        Error - Passwords doesn't match!
-                                    </div>
-                                    <?php
-                                }
-                                elseif (in_array($phototype, $accepted_exte) == true) {
-                                    $newprofileimagename = $email." - Profile image .png";
-                                    $upload_pic = move_uploaded_file($phototmp_name,"images/buyers/profileimages/$newprofileimagename");
-                                    $insert_buyer = mysqli_query($server,"INSERT into buyers values(null,'$firstname','$lastname','$email','$phone','$newprofileimagename','$password')");
-                                    if ($upload_pic && $insert_buyer) {
-                                        ?>
-                                        <div class="succed">
-                                            Congratulations the account is created successfully!
-                                             - <?php echo $email ?> 
-                                        </div>
-                                        <?php
-                                    }
-                                    else {
-                                        ?>
-                                        <div class="failed">
-                                            Error the account creation failed - 
-                                            <b>Contact system support</b>
-                                        </div>
-                                        <?php
-                                    }
                                 }
                                 else {
                                     ?>
                                     <div class="failed">
-                                        Error! The type of image you are trying to upload is invalid.
-                                        Pleased use <br> <b>
-                                            "JPG", "JPEG", "PNG"
-                                        </b>
+                                        Error the account creation failed - 
+                                        <b>Contact system support</b>
                                     </div>
                                     <?php
                                 }
                             }
-                            
-                        ?>
-                    </div>
-
-                    <form class="sign-box" action="" method="POST"  enctype="multipart/form-data">
-                        <p>
-                            Firstname
-                        </p>
-                        <p>
-                            <input type="text" name="firstN" class="text-box" placeholder="Type..." required>
-                        </p>
-                        <p>
-                            Lastname
-                        </p>
-                        <p>
-                            <input type="text" name="lastN" class="text-box" placeholder="Type..." required>
-                        </p>
-                        <p>
-                            Email address
-                        </p>
-                        <p>
-                            <input type="email" name="emailA" class="text-box" placeholder="Type..." required>
-                        </p>
-                        <p>
-                            Phone number
-                        </p>
-                        <p>
-                            <input type="number" name="phoneN" class="text-box" placeholder="Type..." required>
-                        </p>
-                        <p>
-                           Profile picture
-                        </p>
-                        <p>
-                            <input type="file" name="profilepicture" id="" class="text-box" required>
-                        </p>
-                        <p>
-                            Password
-                        </p>
-                        <p>
-                            <input type="password" class="text-box" name="passW" placeholder="Type...">
-                        </p>
-                        <p>
-                            Retype Password
-                        </p>
-                        <p>
-                            <input type="password" class="text-box" name="retp"  placeholder="Type...">
-                        </p>
-                        <div class="btnz">
-                            <button type="submit" name="signupbtn" class="signup-Btn">
-                                <i class="fa fa-check"></i>
-                                Signup
-                            </button>
-                            <button type="reset" class="clear-Btn">
-                                <i class="fa fa-window-close"></i>
-                                Clear form
-                            </button>
-                        </div>
-                    </form>
+                            else {
+                                ?>
+                                <div class="failed">
+                                    Error! The type of image you are trying to upload is invalid.
+                                    Pleased use <br> <b>
+                                        "JPG", "JPEG", "PNG"
+                                    </b>
+                                </div>
+                                <?php
+                            }
+                        }
+                        
+                    ?>
                 </div>
+
+                <form class="sign-box" action="" method="POST"  enctype="multipart/form-data">
+                    <p>
+                        Firstname
+                    </p>
+                    <p>
+                        <input type="text" name="firstN" class="text-box" placeholder="Type..." required>
+                    </p>
+                    <p>
+                        Lastname
+                    </p>
+                    <p>
+                        <input type="text" name="lastN" class="text-box" placeholder="Type..." required>
+                    </p>
+                    <p>
+                        Email address
+                    </p>
+                    <p>
+                        <input type="email" name="emailA" class="text-box" placeholder="Type..." required>
+                    </p>
+                    <p>
+                        Phone number
+                    </p>
+                    <p>
+                        <input type="number" name="phoneN" class="text-box" placeholder="Type..." required>
+                    </p>
+                    <p>
+                        Profile picture
+                    </p>
+                    <p>
+                        <input type="file" name="profilepicture" id="" class="text-box" required>
+                    </p>
+                    <p>
+                        Password
+                    </p>
+                    <p>
+                        <input type="password" class="text-box" name="passW" placeholder="Type...">
+                    </p>
+                    <p>
+                        Retype Password
+                    </p>
+                    <p>
+                        <input type="password" class="text-box" name="retp"  placeholder="Type...">
+                    </p>
+                    <p>
+                        <button type="submit" name="signupbtn" class="signup-Btn">
+                            <i class="fa fa-plus-square"></i>
+                            Signup
+                        </button>
+                    </p>
+                </form>
             </div>
-            <?php
-                include('php/big-footer.php'); include('php/developer-footer.php');
-            ?>
         </div>
+        <?php
+            include('php/big-footer.php'); include('php/developer-footer.php');
+        ?>
     </div>
 </body>
 </html>
